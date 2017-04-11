3aac33dadff3b6768861511331c56a083cd9245d
负心人 <1978027531@qq.com>
2017-04-11 20:13:51 +0800

    Update config.php

--- bk/Application/Common/Conf/config.php
+++ bk/Application/Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	// 'URL_MODEL' => 2,
  //    //数据库配置信息
  //    'DB_TYPE'   => 'mysql', // 数据库类型
- //    'DB_HOST'   => '115.29.42.143', // 服务器地址
+ //    'DB_HOST'   => '', // 服务器地址
  //    'DB_NAME'   => 'bk', // 数据库名
  //    'DB_USER'   => 'root', // 用户名
- //    'DB_PWD'    => '132465', // 密码
+ //    'DB_PWD'    => '', // 密码
  //    'DB_PORT'   => 3306, // 端口
  //    'DB_PREFIX' => 'bk_', // 数据库表前缀
  //    'DB_CHARSET'=> 'utf8', // 字符集
